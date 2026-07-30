54e4c955c23c04ce29846935328d1392d55cf36b
Dharam Nandan <nandandharam@gmail.com>
2026-07-30 18:54:00 +0530

    Learn Python Operators

---
+++ python-basics/operators_practice.py
@@ -0,0 +1,13 @@
+name = "dharam"
+age =32
+experience = 10
+print(age * experience)
+print(type(age * experience))
+print(name * experience)
+print(age * 2)
+print(age / experience)
+print(type(age /4))
+print(age // experience)
+print(type(age // 4))
+print(age % 10)
+print(type(age % 10))
