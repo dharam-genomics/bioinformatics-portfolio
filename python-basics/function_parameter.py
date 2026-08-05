06bb7b11c531a31e81060beb8edbf0a4f7056e29
Dharam Nandan <nandandharam@gmail.com>
2026-08-05 18:39:10 +0530

    Learn Functions with parameters

---
+++ python-basics/function_parameter.py
@@ -0,0 +1,12 @@
+name = input("Enter your Name: ")
+age = input("Enter your age: ")
+def greet(name, age):
+    print("Welcome ", name, "Your age is: ", age)
+
+greet(name, age)
+num1 = int(input("Enter the 1st number: "))
+num2 = int(input("Enter the 2nd number: "))
+def sum(a, b):
+    return a + b
+add = sum(num1, num2)
+print("Addition of" , num1, "&", num2, "is", add)
