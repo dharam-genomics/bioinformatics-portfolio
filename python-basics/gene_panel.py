0aa46882c0c99a904106878ce2cf3cc68111e9e7
Dharam Nandan <nandandharam@gmail.com>
2026-08-05 18:45:02 +0530

    Add gene panel list example

--- python-basics/gene_panel.py
+++ python-basics/gene_panel.py
@@ -2,3 +2,6 @@ genes = ["CHM", "CD247", "DKC1", "HAS1", "CA4"]
 for gene in genes:
     print(gene)
 print("Total genes: ", len(genes))
+
+print("1st gene in the list is:", genes[0])
+print("Last gene in the list is:", genes[4])
