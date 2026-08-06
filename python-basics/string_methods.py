3a1409f4999d3b5277f062db2834cfe8328d54b3
Dharam Nandan <nandandharam@gmail.com>
2026-08-06 17:31:42 +0530

    Learn string method and soem built-in method

---
+++ python-basics/string_methods.py
@@ -0,0 +1,8 @@
+sequence = "atgcgatta"
+print("The same Sequence: ", sequence)
+print("The Sequence in uppercase: ", sequence.upper())
+upper = sequence.upper()
+print("The Sequence in lowercase: ", sequence.lower())
+print("The count of A in Sequence: ", upper.count("A"))
+print("Sequence replaceing T with U: ", upper.replace("T", "U"))
+print("The index of 1st G in the Sequence: ", upper.find("G"))
