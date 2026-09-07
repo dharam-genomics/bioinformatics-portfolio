6ccff2a5aa73da48f2f2d7a65f7b5907052b599a
Dharam Nandan <nandandharam@gmail.com>
2026-09-07 18:48:16 +0530

    Practice the Return function

--- python-basics/calculate_fastq_reads.py
+++ python-basics/calculate_fastq_reads.py
@@ -1,6 +1,7 @@
 def calculate_reads(lines):
-    total_reads = lines // 4
-    return total_reads
+    return lines // 4
+    #total_reads = lines // 4
+    #return total_reads
 
 get_reads = calculate_reads(20)
 print("Total Reads:", get_reads)
